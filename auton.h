ef0852e702d877ad6beb1922e86f6e01aa585796
thejrcrafter <mkirsch72@ymail.com>
2015-01-18 17:51:43 -0500

    Setup for team separation

---
+++ auton.h
@@ -0,0 +1,25 @@
+const int BLU_AUTO=0;
+const int RED_AUTO=1;
+const int BLU_POLE=2;
+const int RED_POLE=3;
+int Program=0;
+
+void autonRedPole()
+{
+
+}
+
+void autonBluPole()
+{
+
+}
+
+void autonRedAuto()
+{
+	
+}
+
+void autonBluAuto()
+{
+	
+}
